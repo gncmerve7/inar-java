bc7208bb4ee4d14b2714c1c540bb29f648e73c37
gncmerve7 <gncmerve7@gmail.com>
2023-09-07 16:50:32 -0400

    updated

---
+++ src/week_05/live_class/Practice03.java
@@ -0,0 +1,28 @@
+package week_05.live_class;
+
+import java.util.Scanner;
+
+public class Practice03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int magicNumber = (int) (Math.random() * 101);
+        int yourGuess;
+
+        System.out.println("Guess a magic number between 0 and 100");
+
+        while(true){
+            System.out.print("Enter your guess : ");
+            yourGuess = input.nextInt();
+            if(yourGuess>magicNumber){
+                System.out.println("Your guess is too high");
+            } else if (yourGuess<magicNumber) {
+                System.out.println("Your guess is too low");
+            }else{
+                System.out.println("Yes, the number is "+magicNumber);
+                break;
+            }
+        }
+    }
+
+}
