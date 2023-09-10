7de38f0894e55ad1be0c8068b63c9852038645b7
gncmerve7 <gncmerve7@gmail.com>
2023-09-10 15:00:56 -0400

    update

--- src/week_05/assignments/Question_05_40.java
+++ src/week_05/assignments/Question_05_40.java
@@ -4,7 +4,7 @@ public class Question_05_40 {
     public static void main(String[] args) {
         int head = 0;
         int tail = 0;
-        System.out.println("Flipping a coin one millon times..");
+        System.out.println("Flipping a coin one million times..");
 
         for (int flipsNum = 0; flipsNum <= 1000000; flipsNum++) {
 
