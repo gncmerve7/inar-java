6b395462a2243b12b3504cb65ae6c21e02d3c202
gncmerve7 <gncmerve7@gmail.com>
2023-11-15 23:03:10 -0500

    update

--- src/week_11/assignments/Question_11_13.java
+++ src/week_11/assignments/Question_11_13.java
@@ -21,12 +21,8 @@ public class Question_11_13 {
 
         for (int i = 0; i < list.size() - 1; i++) {
             if (list.get(i+1).equals(list.get(i))) {
-                // 1,1,3,3,3,3,4,5,6,6
-                // 0 1 2 3 4 5 6 7 8 9 -0
-                // 1,3,3,3,3,4,5,6,6
-                // 0 1 2 3 4 5 6 7 8 9 -1
+
                 list.remove(i+1);
-               // list.set(i-1, list.get(i+1));
 
             }
         }
