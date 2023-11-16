e5e6f751b279c1ec0d8ec6bc87de4cbfb7938f50
gncmerve7 <gncmerve7@gmail.com>
2023-11-16 10:10:17 -0500

    update

--- src/week_11/assignments/Question_11_07.java
+++ src/week_11/assignments/Question_11_07.java
@@ -7,17 +7,25 @@ public class Question_11_07 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a list of integers ending with 0;");
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>(10);
+        int element;
         do {
-            list.add(scan.nextInt());
-        } while (scan.nextInt() != 0);
-
-        System.out.println("Before shuffling:\n"+list);
+            element = scan.nextInt();
+            if (element != 0) {
+                list.add(element);
+            }
+        } while (element != 0);
+        System.out.println("Before shuffling:\n" + list);
         shuffle(list);
-        System.out.println("After shuffling:\n"+list);
+        System.out.println("After shuffling:\n" + list);
     }
 
     public static void shuffle(ArrayList<Integer> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            int j = (int) (Math.random() * list.size());
+            int temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
+        }
     }
 }
