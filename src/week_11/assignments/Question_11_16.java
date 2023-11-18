359d44c54e4097d52f9a21d5b0ad397739d460e6
gncmerve7 <gncmerve7@gmail.com>
2023-11-18 06:35:03 -0500

    update

--- src/week_11/assignments/Question_11_16.java
+++ src/week_11/assignments/Question_11_16.java
@@ -8,29 +8,30 @@ public class Question_11_16 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int userAnswer;
-        int correctAnswer;
-
         int num1 = (int) (Math.random() * 10);
         int num2 = (int) (Math.random() * 10);
         System.out.println("What is " + num1 + " + " + num2 + "?");
+
+        ArrayList<Integer> answers = new ArrayList<>();
+        int userAnswer = scan.nextInt();
+        int result = num1 + num2;
+        answers.add(userAnswer);
+
         do {
-            ArrayList<Integer> result = new ArrayList<>();
-            userAnswer = scan.nextInt();
-            correctAnswer = num1 + num2;
-            result.add(userAnswer);
-            for (int i = 0; i < result.size(); i++) {
-                if (result.get(i) != correctAnswer) {
+
+            for (int i = 0; i < answers.size(); i++) {
+                if (answers.get(i) != result) {
                     System.out.println("Wrong answer. Try again. What is " + num1 + " + " + num2 + "?");
-                    int userAnswer2 = scan.nextInt();
-                    System.out.println("Wrong answer. Try again. What is " + num1 + " + " + num2 + "?");
-                    if (userAnswer2 != correctAnswer && result.contains(userAnswer2)) {
+                    userAnswer = scan.nextInt();
+                    if (answers.get(i) != result && answers.contains(userAnswer)) {
                         System.out.println("You already entered " + userAnswer);
-                        System.out.println("Wrong answer. Try again. What is " + num1 + " + " + num2 + "?");
                     }
+                    answers.add(userAnswer);
                 }
             }
-        } while (num1 + num2 != userAnswer);
+        }
+        while (userAnswer != result);
+
         System.out.println("You got it.");
     }
 }
